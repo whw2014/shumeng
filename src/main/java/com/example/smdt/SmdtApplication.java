0db05de5e727846fecfcbbf07f76ca61c9816e8e
Your Name <you@example.com>
2019-07-29 11:22:02 +0800

    first commit

---
+++ src/main/java/com/example/smdt/SmdtApplication.java
@@ -0,0 +1,13 @@
+package com.example.smdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmdtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/smdt/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.smdt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/abc")
+    public void getInfo(){
+        System.out.println("ascascs");
+    }
+}
